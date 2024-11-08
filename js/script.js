dffba48b1d68012583922d66201b2c4b235610b6
AhmedHany6 <ah7934124@gmail.com>
2024-11-08 23:37:40 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings: ["","Web Developer", "Programmer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
